086fd390b14d803326b471b743ce362ed0bdb4b9
Toby <toby@tfaucher.com>
2025-06-16 18:00:15 -0600

    Added some mini stuff

--- lua/Toby/plugins/mini.lua
+++ lua/Toby/plugins/mini.lua
@@ -43,4 +43,66 @@ return {
             end, { desc = "Toggle into currently opened file"} )
         end
     },
+    {
+        'echasnovski/mini.surround',
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            custom_surroundings = nil,
+            
+            highlight_duration = 300,
+            
+            mappings = {
+                add = 'sa',            -- Add surrounding in Normal and Visual modes
+                delete = 'ds',         -- Delete surrounding
+                find = 'sf',           -- Find surrounding (to the right)
+                find_left = 'sF',      -- Find surrounding (to the left)
+                highlight = 'sh',      -- Highlight surrounding
+                replace = 'sr',        -- Replace surrounding
+                update_n_lines = 'sn', -- Update `n_lines`
+
+                suffix_last = 'l',     -- Suffix to search with "prev" method
+                suffix_next = 'n',     -- Suffix to search with "next" method
+            },
+            n_lines = 20,
+            
+            respect_selection_type = false,
+
+            search_method = 'cover',
+            
+            silent = false,
+        },
+    },
+    {
+        'echasnovski/mini.trailspace',
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            local miniTrailspace = require("mini.trailspace")
+            
+            miniTrailspace.setup({
+                only_in_normal_buffers = true,        
+            })
+
+            vim.keymap.set("n", "<leader>cw", function() miniTrailspace.trim() end, { desc = "Erase Whitespace" } )
+            
+            vim.api.nvim_create_autocmd("CursorMoved", {
+                pattern = "*",
+                callback = function()
+                    require("mini.trailspace").unhighlight()
+                end,
+            })
+        end
+    },
+    {
+        'echasnovski/mini.splitjoin',
+        config = function()
+            local miniSplitjoin = require("mini.splitjoin")
+            
+            miniSplitjoin.setup({
+                mappings = { toggle = "" },
+            }) 
+ 
+            vim.keymap.set( { "n", "x" }, "sj", function() miniSplitJoin.join() end, { desc = "Join arguments" })
+            vim.keymap.set( { "n", "x" }, "sk", function() miniSplitJoin.split() end, { desc = "Split arguments" })
+        end
+    }
 }
